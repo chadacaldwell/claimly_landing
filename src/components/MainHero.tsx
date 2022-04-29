ff90bbdb34ca721e549dcdfcec311bfbc983690b
chad caldwell <chad@chadcaldwell.com>
2022-04-29 16:47:28 -0500

    minor detail updates

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -4,8 +4,9 @@ import config from '../config/index.json';
 
 const MainHero = () => {
   const { mainHero } = config;
+  const { actions } = mainHero;
   return (
-    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+    <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-10 xl:mt-28">
       <div className="sm:text-center lg:text-left">
         <h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl">
           <span className="block xl:inline">{mainHero.title}</span>{' '}
@@ -17,22 +18,18 @@ const MainHero = () => {
           {mainHero.description}
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-          <div className="rounded-md shadow">
-            <a
-              href={mainHero.primaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
-            >
-              {mainHero.primaryAction.text}
-            </a>
-          </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href={mainHero.secondaryAction.href}
-              className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
-            >
-              {mainHero.secondaryAction.text}
-            </a>
-          </div>
+          <ul className={`w-full text-center text-sm`}>
+            {actions.map((action, index) => (
+              <li key={index} className={`py-2`}>
+                <a
+                  href={action.href}
+                  className="w-full flex items-center justify-left px-4 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-2 md:text-lg md:px-4"
+                >
+                  {action.text}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </main>
