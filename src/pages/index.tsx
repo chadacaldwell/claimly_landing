f99a163bd819b4eb3ded743848528810c691249c
chad caldwell <chad@chadcaldwell.com>
2022-04-29 16:14:07 -0500

    got to basic point

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ const App = () => {
       <div className={`relative bg-background`}>
         <div className="max-w-7xl mx-auto">
           <div
-            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+            className={`relative bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
           >
             <Header />
             <MainHero />
