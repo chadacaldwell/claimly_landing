b867261dbe32a812f5be86cccdcfc2566279513d
Chad Caldwell <ccaldwell@fastgem.net>
2022-04-29 19:58:00 -0500

    routing working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import { Link } from 'react-scroll';
+import Link from 'next/link';
 
 import config from '../config/index.json';
 
@@ -22,7 +22,13 @@ const Menu = () => {
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span className="sr-only">{companyName}</span>
-                  <img alt="logo" className="h-16 w-auto sm:h-16" src={logo} />
+                  <Link href="/">
+                    <img
+                      alt="logo"
+                      className="h-16 w-auto sm:h-16"
+                      src={logo}
+                    />
+                  </Link>
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
                   <Popover.Button
@@ -35,26 +41,15 @@ const Menu = () => {
               </div>
             </div>
             <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-              {false &&
-                navigation.map((item) => (
-                  <Link
-                    spy={true}
-                    active="active"
-                    smooth={true}
-                    duration={1000}
-                    key={item.name}
-                    to={item.href}
-                    className="font-medium text-gray-500 hover:text-gray-900"
+              {navigation.map((item, index) => (
+                <Link key={index} href={item.href}>
+                  <a
+                    className={`className="font-medium text-gray-500 hover:text-gray-900`}
                   >
                     {item.name}
-                  </Link>
-                ))}
-              <a
-                href="#"
-                className={`font-medium text-primary hover:text-secondary`}
-              >
-                Call to action
-              </a>
+                  </a>
+                </Link>
+              ))}
             </div>
           </nav>
         </div>
@@ -90,16 +85,10 @@ const Menu = () => {
               </div>
               <div className="px-2 pt-2 pb-3 space-y-1">
                 {navigation.map((item) => (
-                  <Link
-                    spy={true}
-                    active="active"
-                    smooth={true}
-                    duration={1000}
-                    key={item.name}
-                    to={item.href}
-                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
+                  <Link key={item.name} href={item.href}>
+                    <a className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">
+                      {item.name}
+                    </a>
                   </Link>
                 ))}
               </div>
