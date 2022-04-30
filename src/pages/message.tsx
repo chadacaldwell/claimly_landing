57a1f778f89ff9533f5eb48ad8c32a2d8916d3a5
Chad Caldwell <chad@caldwell.cc>
2022-04-30 13:54:32 -0500

    rename

---
+++ src/pages/message.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Analytics from '../components/Analytics';
+import Header from '../components/Header';
+
+const Message = () => {
+  return (
+    <div className={`bg-background grid gap-y-16 overflow-hidden`}>
+      <div className={`relative bg-background`}>
+        <div className="max-w-7xl mx-auto">
+          <div
+            className={`relative bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
+          >
+            <Header />
+          </div>
+        </div>
+      </div>
+      <Analytics />
+    </div>
+  );
+};
+
+export default Message;
