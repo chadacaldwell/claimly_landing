234fbaf920a70287db9a7d87e5e38c1c66f1c687
Chad Caldwell <ccaldwell@fastgem.net>
2022-04-30 17:14:24 -0500

    typeform need to fix overlay

--- src/pages/survey.tsx
+++ src/pages/survey.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Widget } from '@typeform/embed-react';
+
 import Analytics from '../components/Analytics';
 import Header from '../components/Header';
 
@@ -8,10 +10,9 @@ const Survey = () => {
     <div className={`bg-background grid gap-y-16 overflow-hidden`}>
       <div className={`relative bg-background`}>
         <div className="max-w-7xl mx-auto">
-          <div
-            className={`relative bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}
-          >
+          <div>
             <Header />
+            <Widget id="GbTzHtrb" height={500} />
           </div>
         </div>
       </div>
